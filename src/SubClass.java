6bddc39909f2e164c62c7085c5e4080c8eec496a
luqmanHanif <m.lujmanhanif@gmail.com>
2019-10-17 10:08:45 +0700

    First Commit

---
+++ src/SubClass.java
@@ -0,0 +1,11 @@
+
+public class SubClass extends SuperClass {
+
+	public SubClass(String anjay) {
+		// TODO Auto-generated constructor stub
+		super(anjay);
+	}
+	
+
+
+}
---
+++ src/SuperClass.java
@@ -0,0 +1,11 @@
+
+public class SuperClass {
+	
+	public SuperClass(String anjay) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
